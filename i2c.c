a3fe97227c003b8c431efab3a99d374951a309c8
royd4 <royd4@wwu.edu>
2022-05-16 15:42:00 -0700

    bounded buffer!

--- i2c.c
+++ i2c.c
@@ -4,9 +4,40 @@
 
 #define ADDR 0x78
 
-struct buffer buf = {
-  {0}, 0, 0, { 8, 0 }, { 0, 0 }
+static const unsigned char ssd1306_init[] = {
+  0x78,
+  0x80, 0xA8, 0x80, 0x3f,
+  0x80, 0xD3, 0x80, 0x00,
+  0x80, 0x40,
+  0x80, 0xA1,
+  0x80 ,0xC8,
+  0x80, 0xDA, 0x80, 0x12,
+  0x80, 0x81, 0x80, 0x7f,
+  0x80, 0xA4,
+  0x80, 0xA6,
+  0x80, 0xD5, 0x80, 0x80,
+  0x80, 0x8D, 0x80, 0x14,
+  0x80, 0xAF,
+
+  0x80, 0x21, 0x80, 0x00, 0x80, 0x7f,
+  0x80, 0x22, 0x80, 0x00, 0x80, 0x07,
+  0x80, 0x20, 0x80, 0x00,
+
+  0x40,
+  0x55, 0x55, 0x55, 0x55, 0x55,
+  0x55, 0x55, 0x55, 0x55, 0x55
 };
+unsigned i = 0;
+
+/*struct buffer buf = {
+  {0}, 0, 0, { 8, 0 }, { 0, 0 }
+};*/
+
+char buf[8];
+struct buf_info bi = { 0, 0, 0 };
+sem_t empty = { 8, 0 };
+sem_t wait = { 0 , 0 };
+sem_t lock = { 1, 0 };
 
 __attribute__((constructor))
 static void i2c_init() {
@@ -24,18 +55,18 @@ static void i2c_init() {
 }
 
 void buf_push(char c) {
-  sem_down(&buf.empty);
-  buf.buf[buf.first] = c;
-  buf.first++;
-  sem_up(&buf.full);
+  sem_down(&empty);
+  buf[bi.first] = c;
+  bi.first++;
+  bi.used++;
 }
 
 char buf_pop() {
-  if (buf.empty.value != 8) {
-    sem_down(&buf.full);
-    char c = buf.buf[buf.last];
-    buf.last++;
-    sem_up(&buf.empty);
+  if (bi.used != 0) {
+    char c = buf[bi.last];
+    bi.last++;
+    bi.used--;
+    sem_up(&empty);
     return c;
   } else 
     return -1;
@@ -43,9 +74,19 @@ char buf_pop() {
 
 void i2c_write() {
   I2C0.CONSET = (1<<5);
-  I2C0.DAT = 0x78;
+  //I2C0.DAT = 0x78;
+  buf_push(0x78);
+  for (i; i < 8; i++)
+    buf_push(ssd1306_init[i]);
 
-  while (1)
-    __asm("wfi");
+  while (1) {
+    //__asm("wfi");
+    if (i < sizeof(ssd1306_init)) {
+      buf_push(ssd1306_init[i]); 
+    } else {
+      buf_push(0x55);
+    }
+  }
+
 }
 THREAD(disp, i2c_write, 2, 32, 0, 0, 0, 0);
--- i2c.h
+++ i2c.h
@@ -2,13 +2,11 @@
 
 #include "lock.h"
 
-struct buffer {
-  char buf[8];
-  unsigned char first : 3;
-  unsigned char last  : 3;
-  unsigned char       : 2;
-  sem_t empty;
-  sem_t full;
+struct buf_info {
+  unsigned first : 3;
+  unsigned last  : 3;
+  unsigned used  : 4;
+  unsigned       : 0;
 };
 
 void buf_push(char c);
